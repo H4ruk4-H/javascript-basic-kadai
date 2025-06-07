433911ddef817dab4ceddb71aac8f3d63c31ac15
H4ruk4-H <k.969.jn@gmail.com>
2025-06-07 12:56:38 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,5 +1,5 @@
-const fs = require(fs);
-const input  = fs.readFileSync("/dev/stdin,utf-8").trim();
+const fs = require("fs");
+const input  = fs.readFileSync("/dev/stdin","utf-8").trim();
 
 let num = 15;
 
