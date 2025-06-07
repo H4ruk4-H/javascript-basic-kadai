925d2431f3d1a0a61444efb00d506445e6ba425f
H4ruk4-H <k.969.jn@gmail.com>
2025-06-07 13:01:30 +0900

    fsなど削除

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,11 @@
-const fs = require("fs");
-const input  = fs.readFileSync("/dev/stdin","utf-8").trim();
-
-let num = 15;
-
-if(num % 3 === 0){
+let num = 15
+if(num % 3 === 0 && num % 5 === 0){
+    console.log("3と5の倍数です");
+}else if(num % 3 === 0){
     console.log("3の倍数です");
 } else if(num % 5 === 0){
     console.log("5の倍数です");
-} else if(num % 3 === 0 && num % 5 === 0){
-    console.log("3と5の倍数です");
-}else{
+} else {
     console.log(num);
 }
 
