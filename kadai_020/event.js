06c39680fed70e270e272657f614cdeaf3c97683
H4ruk4-H <k.969.jn@gmail.com>
2025-06-08 18:38:40 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,8 +2,5 @@ const textBtn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 textBtn.addEventListener('click', () => {
-    text.innerHTML ='';
-    const child = document.createElement('h2');
-    child.textContent = 'ボタンをクリックしました'
-    text.appendChild(child);
+    text.textContent = 'ボタンをクリックしました'
 });
